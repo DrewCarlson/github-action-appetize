5e8ea065722f097682e51eb59b8f21f1901bd517
Drew Carlson <awcarlson@protonmail.com>
2022-02-03 14:11:25 -0800

    Upload with form data

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
 const core = require('@actions/core');
-import fetch from 'node-fetch';
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
 
-const baseUrl = 'https://api.appetize.io/v1/apps'
+const baseUrl = 'https://TOKEN@api.appetize.io/v1/apps'
 
 const uploadToAppetize = (input) => {
   const { token, publicKey, fileUrl, platform } = input;
-  const fetchOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json, text/plain, */*',
-      Authorization: 'Basic ' + Buffer.from(`${token}:`).toString('base64')
-    },
-    body: JSON.stringify({
-      url: fileUrl,
-      platform: platform
-    })
-  };
   const postfix = (publicKey && publicKey.length > 0) ? `/${publicKey}` : ``;
-  const url = `${baseUrl}${postfix}`;
-  fetch(url, fetchOptions)
-    .then(response => {
+  const url = `${baseUrl.replace('TOKEN', token)}${postfix}`;
+  const stream = fs.createReadStream(fileUrl);
+  const formData = new FormData();
+  formData.append('platform', platform);
+  formData.append('file', stream);
+  axios.post(url, formData, {
+    'maxBodyLength': Infinity,
+    headers: Object.assign({ Accept: 'application/json, text/plain, */*' }, formData.getHeaders())
+  }).then(response => {
       if (response.status == 200) {
         console.log('Success')
-        response.text()
-          .then(jsonResult => JSON.parse(jsonResult))
-          .then(result => core.setOutput('appetize_public_key', result.publicKey))
-          .catch(error => {
-            console.error('response parsing error: ' + error.message);
-          });
+        core.setOutput('appetize_public_key', response.data.publicKey)
       } else {
         response.text().then(text => console.error({ 'error': text }));
         throw new Error(`RequestError (${response.status}) : ${response.statusText}`);
       }
     }).catch(error => {
-      const message = `
-${error.message}
-${JSON.stringify(fetchOptions.headers)}
-${fetchOptions.body}
-`
-      console.error(message);
-      core.setFailed(message);
+      console.error(error.message);
+      core.setFailed(error.message);
     });
 };
 
 try {
   const token = core.getInput('APPETIZE_TOKEN');
-  const publickKey = core.getInput('PUBLICKEY');
+  const publicKey = core.getInput('PUBLICKEY');
   const fileUrl = core.getInput('FILE_URL');
   const platform = core.getInput('PLATFORM');
-  uploadToAppetize({ token, publickKey, fileUrl, platform });
+  uploadToAppetize({ token, publicKey, fileUrl, platform });
 } catch (error) {
   core.setFailed(error.message);
 }
